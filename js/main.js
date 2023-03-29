e1e52df2b8a8a75ca404fcb7e85ea9931f6e610a
Londy <118508725+Londygit1770@users.noreply.github.com>
2023-03-29 23:27:26 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.play_btn').addEventListener('click', function(e) {
+  e.target.closest('.play_btn').classList.toggle('play_btn--toggle');
+})
+let menuBtn = document.querySelector('.play_btn');
+let menu = document.querySelector('.header-two');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+})
+AOS.init({
+  once: true
+});
